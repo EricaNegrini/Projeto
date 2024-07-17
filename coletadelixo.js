11c90e4102d7f0efbddbc3b5cfc9d10c3afc37d3
EricaNegrini <333ericarosa333@gmai.com>
2024-07-17 19:48:56 -0300

    Projeto

---
+++ coletadelixo.js
@@ -0,0 +1,7 @@
+function cartaruma () {
+    document.getElementById("cidadetaruma").innerHTML = "<br><iframe src='https://www.google.com/maps/d/u/0/embed?mid=1qQa0_rsWzoVqj5bmp_ZRBbTheVkNG7Q&ehbc=2E312F&noprof=1' width='640' height='480'></iframe> <img src='imagens/coletataruma.png' alt='imagem coleta' width='600' height='500'>";
+}
+
+function teste() {
+    document.getElementById("teste").innerHTML = "<p>tasdfghuiolkjhgfdftyhje</p>";
+}
